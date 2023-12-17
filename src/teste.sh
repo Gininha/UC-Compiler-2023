25b84cd45db3d2b3bcdf04039757d663ef615f4c
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-12-17 19:37:26 +0000

    Update teste.sh

--- src/teste.sh
+++ src/teste.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 ./build.sh
-./uccompiler < meta4/teste_bonk.c > teste_bonk.ll
-lli teste_bonk.ll
-./uccompiler < meta4/first.c > teste_bonk.ll
-lli teste_bonk.ll | diff meta4/first.out -
-./uccompiler < meta4/hello.c > teste_bonk.ll
-lli teste_bonk.ll | diff meta4/hello.out -
-./uccompiler < meta4/sierpinski.c > teste_bonk.ll
-lli teste_bonk.ll | diff meta4/sierpinski.out -
-./uccompiler < meta4/essential.c > teste_bonk.ll
-lli teste_bonk.ll | diff meta4/essential.out -
-./uccompiler < meta4/The_new_omg.c > teste_bonk.ll
-lli teste_bonk.ll
-./uccompiler < meta4/New_test_case.c > teste_bonk.ll
-lli teste_bonk.ll
+./uccompiler < meta4/teste_bonk.c > meta4/teste_bonk.ll
+lli meta4/teste_bonk.ll | diff meta4/teste_bonk.out -
+./uccompiler < meta4/first.c > meta4/first.ll
+lli meta4/first.ll | diff meta4/first.out -
+./uccompiler < meta4/hello.c > meta4/hello.ll
+lli meta4/hello.ll | diff meta4/hello.out -
+./uccompiler < meta4/sierpinski.c > meta4/sierpinski.ll
+lli meta4/sierpinski.ll | diff meta4/sierpinski.out -
+./uccompiler < meta4/essential.c > meta4/essential.ll
+lli meta4/essential.ll | diff meta4/essential.out -
+./uccompiler < meta4/The_new_omg.c > meta4/The_new_omg.ll
+lli meta4/The_new_omg.ll | diff meta4/The_new_omg.out -
+./uccompiler < meta4/New_test_case.c > meta4/New_test_case.ll
+lli meta4/New_test_case.ll | diff meta4/New_test_case.out -
