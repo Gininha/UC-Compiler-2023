90a3a75e48df93c7002116d7b396c356b74a2c7f
Luis <uc2021199102@student.uc.pt>
2023-12-17 16:02:02 +0000

    teste.sh atualizado

--- src/teste.sh
+++ src/teste.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 ./build.sh
-./uccompiler < teste_bonk.c > teste_bonk.ll
+./uccompiler < meta4/teste_bonk.c > teste_bonk.ll
+lli teste_bonk.ll
+./uccompiler < meta4/first.c > teste_bonk.ll
+lli teste_bonk.ll | diff meta4/first.out -
+./uccompiler < meta4/hello.c > teste_bonk.ll
+lli teste_bonk.ll | diff meta4/hello.out -
+./uccompiler < meta4/sierpinski.c > teste_bonk.ll
+lli teste_bonk.ll | diff meta4/sierpinski.out -
+./uccompiler < meta4/essential.c > teste_bonk.ll
+lli teste_bonk.ll | diff meta4/essential.out -
+./uccompiler < meta4/The_new_omg.c > teste_bonk.ll
 lli teste_bonk.ll
