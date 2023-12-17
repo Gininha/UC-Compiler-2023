60411bc408b0b67caa995a2765b23b92dd9ef93b
Gustavo Lima <gustavo.samuel.lima@gmail.com>
2023-12-17 18:08:44 +0000

    yeet

--- src/teste.sh
+++ src/teste.sh
@@ -2,14 +2,14 @@
 ./build.sh
 ./uccompiler < meta4/teste_bonk.c > teste_bonk.ll
 lli teste_bonk.ll
-./uccompiler < meta4/first.c > teste_bonk.ll
-lli teste_bonk.ll | diff meta4/first.out -
-./uccompiler < meta4/hello.c > teste_bonk.ll
-lli teste_bonk.ll | diff meta4/hello.out -
-./uccompiler < meta4/sierpinski.c > teste_bonk.ll
-lli teste_bonk.ll | diff meta4/sierpinski.out -
-./uccompiler < meta4/essential.c > teste_bonk.ll
-lli teste_bonk.ll | diff meta4/essential.out -
+./uccompiler < meta4/first.c > first.ll
+lli first.ll | diff meta4/first.out -
+./uccompiler < meta4/hello.c > hello.ll
+lli hello.ll | diff meta4/hello.out -
+./uccompiler < meta4/sierpinski.c > sierpinski.ll
+lli sierpinski.ll | diff meta4/sierpinski.out -
+./uccompiler < meta4/essential.c > essential.ll
+lli essential.ll | diff meta4/essential.out -
 
-./uccompiler < meta4/New_test_case.c > teste_bonk.ll
-lli teste_bonk.ll
+./uccompiler < meta4/New_test_case.c > New_test_case.ll
+lli New_test_case.ll
